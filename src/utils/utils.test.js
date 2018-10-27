4b6578487c7d4fc7a2033215c79f5c1d0f9caa2c
elbeezy <djlb1108@gmail.com>
2018-10-27 16:53:13 -0400

    add another unit test and improve eslintrc

--- src/utils/utils.test.js
+++ src/utils/utils.test.js
@@ -1,12 +1,20 @@
 import * as Utils from './utils';
 
+describe('`getRGBObj', () => {
+
+  it('puts RGB values from a CSS string into an object', () => {
+    const expected = { red: 5, green: 4, blue: 7 };
+    const result = Utils.getRGBObj('rgb(5, 4, 7)');
+
+    expect(result).toEqual(expected);
+  });
+});
+
 describe('`stringifyRGB`', () => {
 
   it('puts an object with RGB values into a CSS string', () => {
-    const rgb = { red: 5, green: 4, blue: 7};
-
     const expected = 'rgb(5, 4, 7)';
-    const result = Utils.stringifyRGB(rgb);
+    const result = Utils.stringifyRGB({ red: 5, green: 4, blue: 7});
 
     expect(result).toEqual(expected);
   });
